8e043554c2cb456a98a24ecc15f29b6961cc6c90
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-01-19 12:05:54 -0700

    update versions

--- src/test/java/edu/colorado/cires/wod/transformer/ascii2parquet/WodAsciiParquetTransformerTest.java
+++ src/test/java/edu/colorado/cires/wod/transformer/ascii2parquet/WodAsciiParquetTransformerTest.java
@@ -20,7 +20,7 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.Test;
 
-class WodAsciiParquetTransformerTest {
+public class WodAsciiParquetTransformerTest {
 
   private static final int GEOHASH_LENGTH = 3;
 
